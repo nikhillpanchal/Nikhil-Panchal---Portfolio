86b11087697ca2f347cca3ba3c11e24451baeec0
Nikhil Panchal <nikhilpanchal022@gmail.com>
2026-07-25 02:54:08 +0530

    Add responsive mobile navigation

--- script.js
+++ script.js
@@ -139,16 +139,5 @@ const menuToggle = document.getElementById("menuToggle");
 const navbar = document.getElementById("navbar");
 
 menuToggle.addEventListener("click", () => {
-
     navbar.classList.toggle("active");
-
-});
-document.querySelectorAll("#navbar a").forEach(link => {
-
-    link.addEventListener("click", () => {
-
-        navbar.classList.remove("active");
-
-    });
-
 });
